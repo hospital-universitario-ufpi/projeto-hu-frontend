4fe22ea8ed97105d7e1acabf6055721764d287b2
Bruno Ibiapina <brunoibiapina@me.com>
2025-05-26 10:31:06 -0300

    Ajustei um bug qu estava ocorrendo ao digitar algo em exames

--- .next/types/app/layout.ts
+++ .next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: /home/marcos/Área de Trabalho/projeto-hu-frontend/src/app/layout.tsx
+// File: /Users/brunoibiapina/Desktop/projeto-hu-frontend/src/app/layout.tsx
 import * as entry from '../../../src/app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
--- .next/types/app/page.ts
+++ .next/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: /home/marcos/Área de Trabalho/projeto-hu-frontend/src/app/page.tsx
+// File: /Users/brunoibiapina/Desktop/projeto-hu-frontend/src/app/page.tsx
 import * as entry from '../../../src/app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
