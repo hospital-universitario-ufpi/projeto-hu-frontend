219ad7c9f9a2051df8be4cd951ad2b1332da0d1c
Marcos Valdecy Macedo Costa Leite <mvaldecy11@gmail.com>
2025-05-28 16:33:36 -0300

    feat: implement patient form with validation and state management

--- src/store/PacienteStore.ts
+++ src/store/PacienteStore.ts
@@ -1,35 +1,37 @@
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
-import { PacienteCreationDto } from "@/app/interface/dto/paciente/PacienteCreationDto";
 import { PacienteDto } from "@/app/interface/dto/paciente/PacienteDto";
 
 interface PacienteState {
-  pacienteCreationDto: PacienteCreationDto | null;
   pacienteDto: PacienteDto | null;
   pacienteDtoList: PacienteDto[];
-
-  setPacienteCreationDto: (pacienteCreationDto: PacienteCreationDto) => void;
-  clearPacienteCreationDto: () => void;
+  pacienteUpdate: PacienteDto | null;
 
   setPacienteDto: (pacienteDto: PacienteDto) => void;
   clearPacienteDto: () => void;
 
   setPacienteDtoList: (pacienteDtoList: PacienteDto[]) => void;
   clearPacienteDtoList: () => void;
+
+  setPacienteUpdate: (pacienteUpdate: PacienteDto) => void;
+  clearPacienteUpdate: () => void
 }
 
 export const usePacienteStore = create<PacienteState>()(
   persist(
     (set) => ({
-      pacienteCreationDto: null,
       pacienteDto: null,
       pacienteDtoList: [],
-      setPacienteCreationDto: (pacienteCreationDto) => set({ pacienteCreationDto }),
-      clearPacienteCreationDto: () => set({ pacienteCreationDto: null }),
+      pacienteUpdate: null,
+
       setPacienteDto: (pacienteDto) => set({ pacienteDto }),
       clearPacienteDto: () => set({ pacienteDto: null }),
+
       setPacienteDtoList: (pacienteDtoList) => set({ pacienteDtoList }),
       clearPacienteDtoList: () => set({ pacienteDtoList: [] }),
+
+      setPacienteUpdate: (pacienteUpdate) => set({ pacienteUpdate }),
+      clearPacienteUpdate: () => set({pacienteUpdate: null})
     }),
     {
       name: "paciente-store",
